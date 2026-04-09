f76dafd0cf8a20197e67fe968d85b24c36f9c404
nifeng4 <75618288+nifeng4@users.noreply.github.com>
2026-04-09 15:35:18 +0800

    Add data type definitions for IMU, attitude, and PID controller

--- src/types.h
+++ src/types.h
@@ -1,1 +1,24 @@
-// types.h content here
+// src/types.h
+
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef struct {
+    float roll;
+    float pitch;
+    float yaw;
+} Attitude;
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+} IMU;
+
+typedef struct {
+    float kp;
+    float ki;
+    float kd;
+} PID;
+
+#endif // TYPES_H

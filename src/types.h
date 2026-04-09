716dca0345d121c6125e006afc9b9d6aaafe2506
nifeng4 <75618288+nifeng4@users.noreply.github.com>
2026-04-09 15:28:04 +0800

    Add data type definitions header file

---
+++ src/types.h
@@ -0,0 +1,18 @@
+// Data type definitions
+
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+} Vector3;
+
+typedef struct {
+    float roll;
+    float pitch;
+    float yaw;
+} EulerAngles;
+
+#endif // TYPES_H

9182d7a2a00c695bebad5ce81c3849bf8646fb9b
nifeng4 <75618288+nifeng4@users.noreply.github.com>
2026-04-09 15:34:11 +0800

    Add types.h file

--- src/types.h
+++ src/types.h
@@ -1,18 +1,1 @@
-// Data type definitions
-
-#ifndef TYPES_H
-#define TYPES_H
-
-typedef struct {
-    float x;
-    float y;
-    float z;
-} Vector3;
-
-typedef struct {
-    float roll;
-    float pitch;
-    float yaw;
-} EulerAngles;
-
-#endif // TYPES_H
+// types.h content here
